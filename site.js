ae6d4959d27a204e785f2272b5e17f5ebee9779e
lenovo <shaplykon@gmail.com>
2021-01-17 18:05:03 +0300

    Added main page header, footer and background image

---
+++ site.js
@@ -0,0 +1,49 @@
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+    const bg = document.getElementById("background");
+
+
+    if (isInViewport(bg) && isDownwardScrolling()) {
+        const element = document.getElementById("content");
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+    else if (isInViewport(bg) && !isDownwardScrolling()) {
+        const element = document.getElementById("background");
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+
+});
+
+function isInViewport(el) {
+    var position = el.getBoundingClientRect();
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        return true;
+    }
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        return true;
+    }
+}
+
+function isDownwardScrolling(){
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+
+    let isDownward;
+    if (st > lastScrollTop){
+        isDownward = true;
+    } else {
+        isDownward = false;
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+    return isDownward;
+
+}
+
+
