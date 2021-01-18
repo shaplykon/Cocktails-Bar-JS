9095cc452ee159e241ec80084fb2ecb0f1c4f4d1
lenovo <shaplykon@gmail.com>
2021-01-18 19:36:30 +0300

    Replaced manual scrolling with CSS "scroll-snap-type"

--- site.js
+++ site.js
@@ -1,31 +1,38 @@
-var lastScrollTop = 0;
+let lastScrollTop = 0;
 
 window.addEventListener("scroll", function () {
     const bg = document.getElementById("background");
-
-
     if (isInViewport(bg) && isDownwardScrolling()) {
-        const element = document.getElementById("content");
-        element.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
         document.getElementById("header").style.backgroundColor = "black";
+        console.log("Scrolled down");
     }
     else if (isInViewport(bg) && !isDownwardScrolling()) {
-        const element = document.getElementById("background");
-        element.scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
         document.getElementById("header").style.backgroundColor = "transparent";
+        console.log("Scrolled up");
     }
 
 });
 
+function setTransparentColor() {
+    if (!isDown) {
+        const header = document.getElementById("header");
+        let alpha = 1;
+        const id = setInterval(step, 10);
+
+        function step() {
+            if (alpha === 0) {
+                clearInterval(id);
+            } else {
+                alpha = (alpha - 0.1).toFixed(1);
+                header.style.backgroundColor = "RGBA(0, 0, 0, " + alpha + ")";
+            }
+        }
+    }
+}
+
 function isInViewport(el) {
-    var position = el.getBoundingClientRect();
-    if (position.top >= 0 && position.bottom <= window.innerHeight) {
+    const position = el.getBoundingClientRect();
+    if (position.top >= 0 && position.bottom  <= window.innerHeight) {
         return true;
     }
 
@@ -36,13 +43,8 @@ function isInViewport(el) {
 
 function isDownwardScrolling(){
     const st = window.pageYOffset || document.documentElement.scrollTop;
-
     let isDownward;
-    if (st > lastScrollTop){
-        isDownward = true;
-    } else {
-        isDownward = false;
-    }
+    isDownward = st > lastScrollTop;
     lastScrollTop = st <= 0 ? 0 : st;
     return isDownward;
 
