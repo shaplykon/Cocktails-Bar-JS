3d0f8836e43cc9225ffeaff0d41b9fd7a7e7a5bd
lenovo <shaplykon@gmail.com>
2021-01-19 17:16:01 +0300

    Added login and registration pages

--- js/site.js
+++ js/site.js
@@ -4,31 +4,16 @@ window.addEventListener("scroll", function () {
     const bg = document.getElementById("background");
     if (isInViewport(bg) && isDownwardScrolling()) {
         document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("header").style.boxShadow = "0px 15px 8px 0px rgba(34, 60, 80, 0.2)";
         console.log("Scrolled down");
+
     }
     else if (isInViewport(bg) && !isDownwardScrolling()) {
         document.getElementById("header").style.backgroundColor = "transparent";
+        document.getElementById("header").style.boxShadow = "none";
         console.log("Scrolled up");
     }
-
 });
-
-function setTransparentColor() {
-    if (!isDown) {
-        const header = document.getElementById("header");
-        let alpha = 1;
-        const id = setInterval(step, 10);
-
-        function step() {
-            if (alpha === 0) {
-                clearInterval(id);
-            } else {
-                alpha = (alpha - 0.1).toFixed(1);
-                header.style.backgroundColor = "RGBA(0, 0, 0, " + alpha + ")";
-            }
-        }
-    }
-}
 
 function isInViewport(el) {
     const position = el.getBoundingClientRect();
@@ -36,7 +21,7 @@ function isInViewport(el) {
         return true;
     }
 
-    if (position.top < window.innerHeight && position.bottom >= 0) {
+    if (position.top < window.innerHeight && position.bottom > 0) {
         return true;
     }
 }
@@ -47,7 +32,4 @@ function isDownwardScrolling(){
     isDownward = st > lastScrollTop;
     lastScrollTop = st <= 0 ? 0 : st;
     return isDownward;
-
 }
-
-
