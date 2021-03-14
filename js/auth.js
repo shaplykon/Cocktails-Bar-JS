6fa1c15218a87e9a4dfda8e50778491bc1c20a6c
lenovo <shaplykon@gmail.com>
2021-03-14 23:48:43 +0300

    Added Firebase authorization and simple routing using History API

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,50 @@
 document.addEventListener('DOMContentLoaded', ready());
 
+function loginFunction() {
+    const email = document.getElementById("login");
+    const pass = document.getElementById("password");
 
+    const emailText = email.value;
+    const passText = pass.value;
+    const auth = firebase.auth();
 
-function checkboxClicked(){
+    const response = auth.signInWithEmailAndPassword(emailText, passText);
+    handleAuthentication(response);
+}
+
+function registerFunction() {
+    const email = document.getElementById("login");
+    const pass = document.getElementById("password");
+
+    const emailText = email.value;
+    const passText = pass.value;
+    const auth = firebase.auth();
+
+    const response = auth.createUserWithEmailAndPassword(emailText, passText);
+    handleAuthentication(response);
+}
+
+function handleAuthentication(response){
+    response.then(firebaseUser => {
+        if (firebaseUser) {
+            window.history.pushState({}, "/", window.location.origin + "/");
+        }
+    })
+    response.catch(e => {
+        console.log(e.message);
+        //alert(e.message)
+        const errorField = document.getElementById("errorField");
+        errorField.innerHTML = e.message;
+    });
+}
+
+function checkboxClicked() {
     const checkbox = document.getElementById("rules_agreement");
     const button = document.getElementById("sign_button");
-    checkbox.addEventListener('change', function (event) {
+    checkbox.addEventListener('change', function () {
         button.disabled = !checkbox.checked;
     });
-
 }
+
+
 
--- js/site.js
+++ js/site.js
@@ -1,23 +1,102 @@
 let lastScrollTop = 0;
 
+
+window.onload = () => {
+
+    let root = document.getElementById("root");
+
+    let routes= {
+        "/": "index",
+        "/index": index,
+        "/login": login,
+        '/register': register
+    };
+
+
+    let scripts= {
+        "/": "js/index.js",
+        "/index": "js/index.js",
+        "/login": "/js/auth.js",
+        '/register':"js/auth.js"
+    };
+
+
+
+
+    let definedRoutes = Array.from(document.getElementsByClassName("nav-link"));
+
+    let navigate = e => {
+        let route = e.target.attributes[0].value;
+
+        // redirect to the router instance
+        if (!routes.hasOwnProperty(route)) {
+            window.history.pushState({}, '', 'error')
+            root.innerHTML = `<h1 style="margin:auto auto">This route is not Defined</h1>`
+        } else {
+            if(!(routes[route] === index)){
+                showNavbar();
+            }
+            else{
+                hideNavbar();
+            }
+
+            window.history.pushState({}, '', route)
+            root.innerHTML = routes[route]
+            const scriptSrc = scripts[route];
+            const script = document.createElement("script");
+            script.src = scriptSrc
+            root.appendChild(script);
+        }
+    }
+
+    const cards = document.getElementsByClassName("card");
+    cards.forEach(card => {
+        card.addEventListener('onclick', navigate, false)
+    })
+
+    definedRoutes.forEach(route => {
+        route.addEventListener('click', navigate, false)
+    })
+
+    window.onpopstate = () => {
+        navigate(window.location.pathname);
+    }
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBsXotNUfYiDTXl3dJS9VfrXVik33tdxAs",
+        authDomain: "cocktails-bar.firebaseapp.com",
+        projectId: "cocktails-bar",
+        storageBucket: "cocktails-bar.appspot.com",
+        messagingSenderId: "1021982495967",
+        appId: "1:1021982495967:web:e64ccf37bed108b20ac56a"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+}
+
 window.addEventListener("scroll", function () {
     const bg = document.getElementById("background");
     if (isInViewport(bg) && isDownwardScrolling()) {
-        document.getElementById("header").style.backgroundColor = "black";
-        document.getElementById("header").style.boxShadow = "0px 15px 8px 0px rgba(34, 60, 80, 0.2)";
-        console.log("Scrolled down");
-
-    }
-    else if (isInViewport(bg) && !isDownwardScrolling()) {
-        document.getElementById("header").style.backgroundColor = "transparent";
-        document.getElementById("header").style.boxShadow = "none";
-        console.log("Scrolled up");
+        showNavbar();
+    } else if (isInViewport(bg) && !isDownwardScrolling()) {
+        hideNavbar();
     }
 });
 
+function showNavbar(){
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.boxShadow = "0px 15px 8px 0px rgba(34, 60, 80, 0.2)";
+}
+
+function hideNavbar(){
+    document.getElementById("header").style.backgroundColor = "transparent";
+    document.getElementById("header").style.boxShadow = "none";
+}
+
 function isInViewport(el) {
     const position = el.getBoundingClientRect();
-    if (position.top >= 0 && position.bottom  <= window.innerHeight) {
+    if (position.top >= 0 && position.bottom < window.innerHeight) {
         return true;
     }
 
@@ -32,10 +111,6 @@ function isDownwardScrolling(){
     isDownward = st > lastScrollTop;
     lastScrollTop = st <= 0 ? 0 : st;
     return isDownward;
-}
-
-function cardClick(){
-    window.open("detail.html", "_self", false);
 }
 
 
